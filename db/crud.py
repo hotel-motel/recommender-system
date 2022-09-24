862107cc12b6d8f93c57f89b8074413180807060
Omid Reza <omid77.orh@gmail.com>
2022-09-24 11:32:11 +0330

    Update relationships

--- db/crud.py
+++ db/crud.py
@@ -12,3 +12,27 @@ def get_trip(db: Session, trip_id: int):
 
 def get_trips(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.Trip).offset(skip).limit(limit).all()
+
+def get_hotel(db: Session, hotel_id: int):
+    return db.query(models.Hotel).filter(models.Hotel.id == hotel_id).first()
+
+def get_hotels(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Hotel).offset(skip).limit(limit).all()
+
+def get_city(db: Session, city_id: int):
+    return db.query(models.City).filter(models.City.id == city_id).first()
+
+def get_cities(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.City).offset(skip).limit(limit).all()
+
+def get_room(db: Session, room_id: int):
+    return db.query(models.Room).filter(models.Room.id == room_id).first()
+
+def get_rooms(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Room).offset(skip).limit(limit).all()
+
+def get_passenger(db: Session, passenger_id: int):
+    return db.query(models.Passenger).filter(models.Passenger.id == passenger_id).first()
+
+def get_passengers(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Passenger).offset(skip).limit(limit).all()
--- db/schemas.py
+++ db/schemas.py
@@ -1,6 +1,16 @@
 from datetime import date
 from pydantic import BaseModel
 
+class PassengerBase(BaseModel):
+    first_name: str
+    last_name: str
+    national_code:str
+
+class Passenger(PassengerBase):
+    id: int
+    trip_id: int
+    class Config:
+        orm_mode = True
 
 class TripBase(BaseModel):
     start: date
@@ -10,6 +20,21 @@ class TripBase(BaseModel):
 class Trip(TripBase):
     id: int
     creator_id: int
+    room_id: int
+    passengers: list[Passenger] = []
+    class Config:
+        orm_mode = True
+
+class RoomBase(BaseModel):
+    number: int
+    price: int
+    floor: int
+    max_capacity: int
+
+class Room(RoomBase):
+    id: int
+    hotel_id:int
+    trips: list[Trip] = []
     class Config:
         orm_mode = True
 
@@ -19,5 +44,28 @@ class UserBase(BaseModel):
 class User(UserBase):
     id: int
     trips: list[Trip] = []
+    class Config:
+        orm_mode = True
+
+class HotelBase(BaseModel):
+    name: str
+    address: str
+    image: str
+    credit: int
+
+class Hotel(HotelBase):
+    id: int
+    city_id: int
+    rooms: list[Room]=[]
+    class Config:
+        orm_mode = True
+
+class CityBase(BaseModel):
+    name: str
+    image: str
+    hotels: list[Hotel] = []
+
+class City(CityBase):
+    id: int
     class Config:
         orm_mode = True
