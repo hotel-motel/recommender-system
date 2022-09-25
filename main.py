3788fb9581b48bab3b664f94dccb905d659dc231
Omid Reza <omid77.orh@gmail.com>
2022-09-25 23:46:37 +0330

    Replace databases package with sqlalchemy

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
-from sqlalchemy.orm import Session
-from fastapi import Depends, FastAPI
-from db import crud, models, schemas
-from db.database import SessionLocal, engine
+from fastapi import FastAPI
+from dotenv import load_dotenv
+from os import environ
+from databases import Database
 
-models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
+load_dotenv('.env')
 
-# Dependency
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+DATABASE_URL = environ.get('DB_CONNECTION')+'://'+environ.get('DB_USERNAME')+':'+environ.get('DB_PASSWORD')+'@'+environ.get('DB_HOST')+':'+environ.get('DB_PORT')+'/'+environ.get('DB_DATABASE')
+database = Database(DATABASE_URL)
 
-@app.get("/hotels/")
-async def get_recommended_hotels(token : str, db: Session = Depends(get_db)):
-    return {"Message": "Hello World :) "}
+@app.get("/hotels/{user_id}")
+async def get_recommended_hotels(user_id : str):
+    await database.connect()
+
+    query = """
+        SELECT DISTINCT(creator_id)
+        FROM trips
+        WHERE room_id in (
+            SELECT id
+            FROM rooms
+            WHERE hotel_id in (
+                SELECT DISTINCT(hotel_id)
+                FROM rooms
+                WHERE id in (
+                    SELECT room_id
+                    FROM trips
+                    WHERE creator_id="""+user_id+"""
+                )
+            )
+        ) and creator_id <>"""+user_id
+
+    sim_user_ids = await database.fetch_all(query=query)
+    return sim_user_ids
