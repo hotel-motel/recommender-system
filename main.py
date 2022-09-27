8bf3a42def8d8210a1be3563133c09caef69a24b
Omid Reza <omid77.orh@gmail.com>
2022-09-28 00:06:33 +0330

    Optimize queries and return response

--- main.py
+++ main.py
@@ -11,38 +11,34 @@ database = Database(DATABASE_URL)
 
 @app.get("/hotels/{user_id}")
 async def get_recommended_hotels(user_id : str):
+    return_count = 4
     await database.connect()
     query = """
-        SELECT DISTINCT(creator_id)
-        FROM trips
-        WHERE room_id in (
-            SELECT id
-            FROM rooms
-            WHERE hotel_id in (
-                SELECT DISTINCT(hotel_id)
-                FROM trips INNER JOIN rooms ON trips.room_id=room_id
-                WHERE trips.creator_id = """+user_id+"""
-            )
-        )"""
-        # and creator_id <>"""+user_id
+        SELECT DISTINCT(trips.creator_id)
+        FROM trips INNER JOIN rooms ON trips.room_id=rooms.id
+        where rooms.hotel_id in (
+            SELECT DISTINCT(rooms.hotel_id)
+            FROM trips INNER JOIN rooms ON trips.room_id=rooms.id
+            WHERE trips.creator_id = {}
+        )
+    """.format(user_id)
+    # AND trips.creator_id <> {}
     sim_user_ids = await database.fetch_all(query=query)
-    sim_user_ids_query = ""
-    
+    sim_user_ids_query = []
     for u in sim_user_ids:
-        sim_user_ids_query=sim_user_ids_query+str(u.creator_id)+","
-    sim_user_ids_query=sim_user_ids_query[:-1]
+        sim_user_ids_query.append(u.creator_id)
 
     query="""
-        SELECT creator_id, hotel_id
-        FROM trips INNER JOIN rooms ON trips.room_id=rooms.id
-        WHERE creator_id IN ("""+sim_user_ids_query+""")
-         ORDER BY hotel_id
-    """
-    matrix_sim_user_hotels = await database.fetch_all(query=query)
-    weighted = {}
-    for h in matrix_sim_user_hotels:
-        if(h.hotel_id in weighted):
-            weighted[h.hotel_id]=weighted[h.hotel_id]+1
-        else:
-            weighted[h.hotel_id]=1
-    return weighted
+        SELECT hotels.id, hotels.name, hotels.image, hotels.star
+        FROM hotels
+        WHERE hotels.id IN (
+            SELECT rooms.hotel_id
+            FROM trips INNER JOIN rooms ON trips.room_id=rooms.id
+            WHERE creator_id IN {}
+            GROUP BY rooms.hotel_id
+            ORDER BY count(trips.creator_id) DESC
+            LIMIT {}
+        )
+    """.format(tuple(sim_user_ids_query), str(return_count))
+    response = await database.fetch_all(query=query)
+    return response
